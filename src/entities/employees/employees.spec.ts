a472166b0914d092a900297451e918309be17eb9
wagnerlim <wagnerlima0910@gmail.com>
2022-10-06 13:32:10 -0300

    fix: fixes create employee test

--- src/entities/employees/employees.spec.ts
+++ src/entities/employees/employees.spec.ts
@@ -3,15 +3,16 @@ import { Employee } from "./Employees";
 
 test("should be create a employee", () => {
   const employee = new Employee({
-    name: "Joe doe",
+    name: "John Doe",
     birthday: new Date("09/10/1980"),
     document: "458.622.688-98",
+    documentType:'CPF',
     departament: "Technology",
     role: "Developer",
   });
-  expect(employee.birthday).toEqual('John Doe');
+  expect(employee.name).toEqual("John Doe");
   expect(employee.birthday).toEqual(new Date("09/10/1980"));
-  expect(employee.document).toEqual("458.622.688.98");
+  expect(employee.document).toEqual("458.622.688-98");
   expect(employee.departament).toEqual("Technology");
   expect(employee.role).toEqual("Developer");
   expect(employee).toBeInstanceOf(Employee);
