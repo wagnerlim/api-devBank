e6ba968e01266f2a5a38bc67a8adf4545358a664
wagnerlim <wagnerlima0910@gmail.com>
2022-12-08 10:17:17 -0300

    fix: functions

--- src/shared/library/Logger.ts
+++ src/shared/library/Logger.ts
@@ -1,21 +1,24 @@
 import chalk from "chalk";
 
 export default class Logger {
-  public static info = (value: any) =>
+  public static info(value: any) {
     console.log(
       chalk.blue(`${new Date().toLocaleString()} [INFO]`),
       typeof value === "string" ? chalk.blue(value) : value
     );
+  }
 
-  public static warn = (value: any) =>
+  public static warn(value: any) {
     console.log(
       chalk.blue(`${new Date().toLocaleString()} [WARN]`),
       typeof value === "string" ? chalk.yellowBright(value) : value
     );
+  }
 
-  public static error = (value: any) =>
+  public static error(value: any) {
     console.log(
       chalk.blue(`${new Date().toLocaleString()} [ERROR]`),
       typeof value === "string" ? chalk.redBright(value) : value
     );
+  }
 }
